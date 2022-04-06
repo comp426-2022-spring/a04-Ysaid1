91114e284d82105440384dab1273cd1d4b22dc25
Ysaid1 <Ysaid@unc.edu>
2022-04-06 18:33:45 -0400

    checking now

---
+++ server.js
@@ -0,0 +1,183 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan');
+const fs = require('fs')
+//allow reading of json
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+const db = require('./database.js')
+// Require Express.js
+const args = minimist(process.argv.slice(2));
+const port = args.port || process.env.PORT || 5555;
+args['port'];
+args['help']
+args['debug']
+args['log']
+const help = args.help
+const debug = args.debug
+const log = args.log
+//If the command line is help, offer these solutions
+if(help){
+  console.log(`  
+--port	    Set the port number for the server to listen on. Must be an integer
+          between 1 and 65535.
+--debug     If set to \`true\`, creates endlpoints /app/log/access/ which returns
+          a JSON access log from the database and /app/error which throws 
+          an error with the message "Error test successful." Defaults to 
+         \`false\`.
+--log       If set to false, no log files are written. Defaults to true.
+          Logs are always written to database.
+--help	  Return this message and exit.`)
+  process.exit(0)
+}
+//if log is true then write to the file
+if (log) {
+  const accessLog = fs.createWriteStream('access.log', { flags: 'a' });
+  app.use(morgan('combined', { stream: accessLog }));
+};
+
+// Start an app server
+const server = app.listen(port, () => {
+  console.log('App listening on port %PORT%'.replace('%PORT%', port));
+});
+
+//defauly or base endpoint to return everything is normal
+app.get('/app/', (req, res) => {
+  // Respond with status 200
+  res.statusCode = 200;
+  // Respond with status message "OK"
+  res.statusMessage = 'OK';
+  res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
+  res.end(res.statusCode+ ' ' +res.statusMessage)
+});
+
+app.use((req, res, next) => {
+  let logdata = {
+    remoteaddr: req.ip,
+    remoteuser: req.user,
+    time: Date.now(),
+    method: req.method,
+    url: req.url,
+    protocol: req.protocol,
+    httpversion: req.httpVersion,
+    secure: req.secure,
+    status: res.statusCode,
+    referer: req.headers['referer'],
+    useragent: req.headers['user-agent']
+  }
+  const stmt = db.prepare(`INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, 
+    protocol, httpversion, secure, status, referer, useragent) VALUES (?,?,?,?,?,?,?,?,?,?,?)`)
+    const infoToStore = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.secure, logdata.status, logdata.referer, logdata.useragent)
+    res.status(200).json(infoToStore).next();
+})
+//check if debug is true
+if (args.debug == 'true') {
+  app.get('/app/log/access', (req, res) => {
+    try {
+      const stmt = db.prepare('SELECT * FROM accesslog').all()
+      res.status(200).json(stmt)
+      } catch(e) {
+        console.error(e)
+      }
+});
+
+//endpoint for just 1 flip
+app.get('/app/flip/', (req, res) => {
+  //call coin flip function
+  var flip = coinFlip();
+  // Respond with status 200
+  res.status(200).json({ "flip" : flip });
+});
+//endpoint for coin flips given a number
+app.get('/app/flips/:number', (req, res) => {
+  var flips = coinFlips(req.params.number)
+  res.status(200).json({"raw" : flips, "summary" : countFlips(flips)})
+});
+
+//endpoint for calling a flip
+app.get('/app/flip/call/:guess(heads|tails)', (req, res) => {
+  //call the function flip a coin and take in paramater call
+  var callOfFlips = flipACoin(req.params.guess);
+  // Respond with status 200
+  res.status(200).json(callOfFlips);;
+});
+
+//Error handling if Endpoint does not exist
+app.use(function(req, res){
+  res.status(404)
+  res.json({"message":"Endpoint not found (404)"})
+})
+
+
+
+/** Simple coin flip
+ * @param {*}
+ * @returns {string} 
+ */
+function coinFlip() {
+  return Math.random() > 0.5 ? ("heads") : ("tails")
+}
+
+function coinFlips(flips) {
+  var arrayOfFlips = [flips]
+  //loop through each flip to determine if it is heads or not
+  for (var i = 0; i < flips; i++){
+    arrayOfFlips[i] = coinFlip();
+    //get the number of heads and tails
+  }
+  return arrayOfFlips;
+}
+function countFlips(array) {
+  //create a new object to hold number of heads and tails
+  //set heads and tails to 0
+  var countOfHeadsAndTails = {heads: 0, tails: 0}
+  //loop through
+  for (let i = 0; i < array.length; i++){
+    //increment number of heads if it is heads
+    if (array[i] == 'heads'){
+      //increment number of heads
+      countOfHeadsAndTails.heads = countOfHeadsAndTails.heads + 1;
+    }
+    else {
+      //increment number of tails in the object
+      countOfHeadsAndTails.tails = countOfHeadsAndTails.tails + 1;
+    }
+  }
+  //remove  tails if it doesn't exist
+  if (countOfHeadsAndTails.heads == 1){
+    delete countOfHeadsAndTails.tails;
+  }
+  else if (countOfHeadsAndTails.tails == 1){
+    delete countOfHeadsAndTails.heads;
+  }
+  //return the object
+  return countOfHeadsAndTails;
+}
+
+function flipACoin(call) {
+  //Flip a coin to get heads or tails
+  var flippingACoin = coinFlip()
+  //initailze the result of the flip
+  var resultOfFlip =''
+  //check to see if the coin flip was correct
+  if (flippingACoin == call){
+    //if the coin flip matches the call change result to win
+    resultOfFlip = resultOfFlip + 'win';
+  }
+  else {
+    //if coin call does not match the flip, change the result to lose
+    resultOfFlip = resultOfFlip + 'lose'
+  }
+  //create an object to hold the variables
+  var checkResult  = {call:call, flip:'', result : ''};
+  //add the flip result to flip
+  checkResult.flip = flippingACoin;
+  //add the result of the flip to the result object variable
+  checkResult.result = resultOfFlip;
+  if(call == null || call == ""){
+    throw 'No input';
+  }
+  //return it
+  return checkResult;
+}
+}
