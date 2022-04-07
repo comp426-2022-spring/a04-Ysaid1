da6330dcb701f9e7071710d579840e41fcab3f94
Ysaid1 <Ysaid@unc.edu>
2022-04-06 20:19:39 -0400

    changed order

--- server.js
+++ server.js
@@ -58,44 +58,9 @@ if (row == undefined){
 } else {
     console.log('Log Databse exists')
 }
-//check if debug is true
-if (debug) {
-  app.get('/app/log/access', (req, res) => {
-    try {
-      const stmt = db.prepare('SELECT * FROM accesslog').all()
-      res.status(200).json(stmt)
-      } catch(e) {
-        console.error(e)
-      }
-});   
-//end point for error testing
-app.get('/app/error', (req, res) => {
-    throw new Error('Error test worked.')
-});
-};
-
 
 module.exports = db;
 
-app.use((req, res, next) => {
-  let logdata = {
-    remoteaddr: req.ip,
-    remoteuser: req.user,
-    time: Date.now(),
-    method: req.method,
-    url: req.url,
-    protocol: req.protocol,
-    httpversion: req.httpVersion,
-    secure: req.secure,
-    status: res.statusCode,
-    referer: req.headers['referer'],
-    useragent: req.headers['user-agent']
-  }
-  const stmt = db.prepare(`INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, 
-    protocol, httpversion, secure, status, referer, useragent) VALUES (?,?,?,?,?,?,?,?,?,?,?)`)
-    stmt.run(Object.values(logdata));
-    next();
-});
 const server = app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 });
@@ -120,6 +85,43 @@ app.get('/app/flip/call/:guess(heads|tails)', (req, res) => {
     // Respond with status 200
     res.status(200).json(callOfFlips);;
  });
+
+ //check if debug is true
+if (debug) {
+  app.get('/app/log/access', (req, res) => {
+    try {
+      const stmt = db.prepare('SELECT * FROM accesslog').all()
+      res.status(200).json(stmt)
+      } catch(e) {
+        console.error(e)
+      }
+});   
+//end point for error testing
+app.get('/app/error', (req, res) => {
+    throw new Error('Error test worked.')
+});
+};
+
+
+app.use((req, res, next) => {
+  let logdata = {
+    remoteaddr: req.ip,
+    remoteuser: req.user,
+    time: Date.now(),
+    method: req.method,
+    url: req.url,
+    protocol: req.protocol,
+    httpversion: req.httpVersion,
+    secure: req.secure,
+    status: res.statusCode,
+    referer: req.headers['referer'],
+    useragent: req.headers['user-agent']
+  }
+  const stmt = db.prepare(`INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, 
+    protocol, httpversion, secure, status, referer, useragent) VALUES (?,?,?,?,?,?,?,?,?,?,?)`)
+    stmt.run(Object.values(logdata));
+    next();
+});
 
 //Error handling if Endpoint does not exist
 app.use(function(req, res){
