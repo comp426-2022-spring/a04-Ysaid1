931e776e673d1cb01317777b2fe3a63320ac6832
Ysaid1 <Ysaid@unc.edu>
2022-04-06 21:09:43 -0400

    logging = true

--- server.js
+++ server.js
@@ -1,3 +1,74 @@
+/** Simple coin flip
+ * @param {*}
+ * @returns {string} 
+ */
+ function coinFlip() {
+  return Math.random() > 0.5 ? ("heads") : ("tails")
+}
+
+function coinFlips(flips) {
+  var arrayOfFlips = [flips]
+  //loop through each flip to determine if it is heads or not
+  for (var i = 0; i < flips; i++){
+    arrayOfFlips[i] = coinFlip();
+    //get the number of heads and tails
+  }
+  return arrayOfFlips;
+}
+function countFlips(array) {
+  //create a new object to hold number of heads and tails
+  //set heads and tails to 0
+  var countOfHeadsAndTails = {heads: 0, tails: 0}
+  //loop through
+  for (let i = 0; i < array.length; i++){
+    //increment number of heads if it is heads
+    if (array[i] == 'heads'){
+      //increment number of heads
+      countOfHeadsAndTails.heads = countOfHeadsAndTails.heads + 1;
+    }
+    else {
+      //increment number of tails in the object
+      countOfHeadsAndTails.tails = countOfHeadsAndTails.tails + 1;
+    }
+  }
+  //remove  tails if it doesn't exist
+  if (countOfHeadsAndTails.heads == 1){
+    delete countOfHeadsAndTails.tails;
+  }
+  else if (countOfHeadsAndTails.tails == 1){
+    delete countOfHeadsAndTails.heads;
+  }
+  //return the object
+  return countOfHeadsAndTails;
+}
+
+function flipACoin(call) {
+  //Flip a coin to get heads or tails
+  var flippingACoin = coinFlip()
+  //initailze the result of the flip
+  var resultOfFlip =''
+  //check to see if the coin flip was correct
+  if (flippingACoin == call){
+    //if the coin flip matches the call change result to win
+    resultOfFlip = resultOfFlip + 'win';
+  }
+  else {
+    //if coin call does not match the flip, change the result to lose
+    resultOfFlip = resultOfFlip + 'lose'
+  }
+  //create an object to hold the variables
+  var checkResult  = {call:call, flip:'', result : ''};
+  //add the flip result to flip
+  checkResult.flip = flippingACoin;
+  //add the result of the flip to the result object variable
+  checkResult.result = resultOfFlip;
+  if(call == null || call == ""){
+    throw 'No input';
+  }
+  //return it
+  return checkResult;
+}
+
 const express = require('express')
 const app = express()
 const morgan = require('morgan');
@@ -24,9 +95,11 @@ if(help != null){
   process.exit(0)
 }
 //if log is true then write to the file
-if (log) {
-  const accesslog = fs.createWriteStream('access.log', {flags: 'a'})
-  app.use(morgan('FORMAT', {stream: accesslog}))
+if(log === true) {
+  //allow to write on file
+  const accessLog = fs.createWriteStream('access.log', { flags: 'a' })
+  // Set up the middleware
+  app.use(morgan('combined', { stream: accessLog }))
 }
 //check port
 if (port > 65535 || port < 1) {
@@ -129,73 +202,3 @@ app.use(function(req, res){
     res.type("text/plain")
 })
 
-/** Simple coin flip
- * @param {*}
- * @returns {string} 
- */
- function coinFlip() {
-  return Math.random() > 0.5 ? ("heads") : ("tails")
-}
-
-function coinFlips(flips) {
-  var arrayOfFlips = [flips]
-  //loop through each flip to determine if it is heads or not
-  for (var i = 0; i < flips; i++){
-    arrayOfFlips[i] = coinFlip();
-    //get the number of heads and tails
-  }
-  return arrayOfFlips;
-}
-function countFlips(array) {
-  //create a new object to hold number of heads and tails
-  //set heads and tails to 0
-  var countOfHeadsAndTails = {heads: 0, tails: 0}
-  //loop through
-  for (let i = 0; i < array.length; i++){
-    //increment number of heads if it is heads
-    if (array[i] == 'heads'){
-      //increment number of heads
-      countOfHeadsAndTails.heads = countOfHeadsAndTails.heads + 1;
-    }
-    else {
-      //increment number of tails in the object
-      countOfHeadsAndTails.tails = countOfHeadsAndTails.tails + 1;
-    }
-  }
-  //remove  tails if it doesn't exist
-  if (countOfHeadsAndTails.heads == 1){
-    delete countOfHeadsAndTails.tails;
-  }
-  else if (countOfHeadsAndTails.tails == 1){
-    delete countOfHeadsAndTails.heads;
-  }
-  //return the object
-  return countOfHeadsAndTails;
-}
-
-function flipACoin(call) {
-  //Flip a coin to get heads or tails
-  var flippingACoin = coinFlip()
-  //initailze the result of the flip
-  var resultOfFlip =''
-  //check to see if the coin flip was correct
-  if (flippingACoin == call){
-    //if the coin flip matches the call change result to win
-    resultOfFlip = resultOfFlip + 'win';
-  }
-  else {
-    //if coin call does not match the flip, change the result to lose
-    resultOfFlip = resultOfFlip + 'lose'
-  }
-  //create an object to hold the variables
-  var checkResult  = {call:call, flip:'', result : ''};
-  //add the flip result to flip
-  checkResult.flip = flippingACoin;
-  //add the result of the flip to the result object variable
-  checkResult.result = resultOfFlip;
-  if(call == null || call == ""){
-    throw 'No input';
-  }
-  //return it
-  return checkResult;
-}
